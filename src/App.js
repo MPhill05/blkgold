cc1c96998108ddf0e340c186ca46d35dc2322887
Mark Phillips <mphill05@gmail.com>
2020-11-30 20:56:54 -0800

    created pages and set routing for site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import MusicPage from './pages/music/music.component';
+import TourPage from './pages/tour/tour.component';
+import ContactPage from './pages/contact/contact.component';
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/music' component={MusicPage} />
+        <Route path='/tour' component={TourPage} />
+        <Route path='/contact' component={ContactPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/music/music.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './music.styles.scss';
+
+const MusicPage = () => (
+    <div>
+        Music Page
+    </div>
+)
+
+export default MusicPage;
